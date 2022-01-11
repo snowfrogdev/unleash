21428bd1b9793bd969bc06c2500c19c841d678a5
Thomas Heartman <thomas.heartman+git@gmail.com>
2022-01-11 13:25:31 +0100

    docs: add introduction as separate entry

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,3 +1,4 @@
+
 /**
  * Creating a sidebar enables you to:
  - create an ordered group of docs
@@ -10,73 +11,76 @@
  */
 
 module.exports = {
-    documentation: {
-        'Tutorials': [            'user_guide/index'],
-        'Reference documentation': [            'user_guide/index'],
-        'Getting started': [
-            'user_guide/index',
-            'user_guide/quickstart',
-            'user_guide/important-concepts',
-            'user_guide/v4-whats-new',
-            'user_guide/create_feature_toggle',
-            'user_guide/activation_strategy',
-            'user_guide/control_rollout',
-            'user_guide/projects',
-            'user_guide/environments',
-            'user_guide/unleash_context',
-            'user_guide/user-management',
-            'user_guide/rbac',
-            'user_guide/api-token',
-            'user_guide/technical_debt',
-        ],
-        'Unleash SDKs': [
-            'sdks/index',
-            'sdks/java_sdk',
-            'sdks/node_sdk',
-            'sdks/dot_net_sdk',
-            'sdks/go_sdk',
-            'sdks/python_sdk',
-            'sdks/ruby_sdk',
-            'sdks/php_sdk',
-            'sdks/unleash-proxy',
-            'sdks/android_proxy_sdk',
-            'sdks/proxy-javascript',
-            'sdks/proxy-react',
-            'sdks/proxy-ios',
-            { type: 'link', label: 'Community SDKs', href: '/sdks#community-sdks'}
-        ],
-        Addons: [
-            'addons/index',
-            'addons/webhook',
-            'addons/slack',
-            'addons/teams',
-            'addons/datadog',
-        ],
-        Advanced: [
-            'advanced/strategy_constraints',
-            'advanced/custom_activation_strategy',
-            'advanced/feature_toggle_types',
-            'advanced/toggle_variants',
-            'advanced/stickiness',
-            'advanced/archived_toggles',
-            'advanced/audit_log',
-            'advanced/api_access',
-            'advanced/tags',
-            'advanced/enterprise-authentication',
-            'advanced/sso-open-id-connect',
-            'advanced/sso-saml',
-            'advanced/sso-saml-keycloak',
-            'advanced/sso-google',
-        ],
-        'Topic guides': [
-            'topics/a-b-testing'
-        ],
-        "How-to guides": [
-            "how-to/how-to-add-strategy-constraints",
-            "how-to/how-to-define-custom-context-fields",
-            "how-to/how-to-use-custom-strategies",
-        ]
-    },
+    documentation: [
+        'user_guide/index',
+        {
+            'Tutorials': [            'user_guide/index'],
+            'Reference documentation': [            'user_guide/index'],
+            'Getting started': [
+                'user_guide/index',
+                'user_guide/quickstart',
+                'user_guide/important-concepts',
+                'user_guide/v4-whats-new',
+                'user_guide/create_feature_toggle',
+                'user_guide/activation_strategy',
+                'user_guide/control_rollout',
+                'user_guide/projects',
+                'user_guide/environments',
+                'user_guide/unleash_context',
+                'user_guide/user-management',
+                'user_guide/rbac',
+                'user_guide/api-token',
+                'user_guide/technical_debt',
+            ],
+            'Unleash SDKs': [
+                'sdks/index',
+                'sdks/java_sdk',
+                'sdks/node_sdk',
+                'sdks/dot_net_sdk',
+                'sdks/go_sdk',
+                'sdks/python_sdk',
+                'sdks/ruby_sdk',
+                'sdks/php_sdk',
+                'sdks/unleash-proxy',
+                'sdks/android_proxy_sdk',
+                'sdks/proxy-javascript',
+                'sdks/proxy-react',
+                'sdks/proxy-ios',
+                { label: 'Community SDKs', type: 'link', href: '/sdks#community-sdks'}
+            ],
+            Addons: [
+                'addons/index',
+                'addons/webhook',
+                'addons/slack',
+                'addons/teams',
+                'addons/datadog',
+            ],
+            Advanced: [
+                'advanced/strategy_constraints',
+                'advanced/custom_activation_strategy',
+                'advanced/feature_toggle_types',
+                'advanced/toggle_variants',
+                'advanced/stickiness',
+                'advanced/archived_toggles',
+                'advanced/audit_log',
+                'advanced/api_access',
+                'advanced/tags',
+                'advanced/enterprise-authentication',
+                'advanced/sso-open-id-connect',
+                'advanced/sso-saml',
+                'advanced/sso-saml-keycloak',
+                'advanced/sso-google',
+            ],
+            'Topic guides': [
+                'topics/a-b-testing'
+            ],
+            "How-to guides": [
+                "how-to/how-to-add-strategy-constraints",
+                "how-to/how-to-define-custom-context-fields",
+                "how-to/how-to-use-custom-strategies",
+            ]
+        }
+    ],
     api: {
         Introduction: [
             'api/index',
